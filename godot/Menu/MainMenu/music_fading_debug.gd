b102caad307bef4804c24b4a4aa60cc84d954a3d
Paul Norberger <panor@fastmail.com>
2020-10-04 05:36:33 +0200

    Changed debug test to use new crossfade method

--- godot/Menu/MainMenu/music_fading_debug.gd
+++ godot/Menu/MainMenu/music_fading_debug.gd
@@ -3,17 +3,15 @@ extends Control
 onready var tween = $Tween
 
 func _on_StartMusicButton_pressed():
-	SoundController.pub_play_effect("res://testmusic/song1.ogg", 0)
-	SoundController.pub_play_effect("res://testmusic/song2.ogg", 1, -100)
-
+	SoundController.pub_play_music("res://testmusic/song1.ogg", 0)
+	SoundController.pub_play_music("res://testmusic/song2.ogg", 1, -100)
 
 func _on_CrossfadeButton_pressed():
 	var sound_layer_0 := SoundController.pub_get_audio_effect_player(0)
 	var sound_layer_1 := SoundController.pub_get_audio_effect_player(1)
 	
-	# sound_layer_0.volume_db = -100
-	#sound_layer_1.volume_db = -100
+	# tween.interpolate_property(sound_layer_0, "volume_db", null, -100, 1, Tween.TRANS_CIRC)
+	# tween.interpolate_property(sound_layer_1, "volume_db", null, -12, 2, Tween.TRANS_CIRC)
+	# tween.start()
 	
-	tween.interpolate_property(sound_layer_0, "volume_db", null, -100, 1, Tween.TRANS_CIRC)
-	tween.interpolate_property(sound_layer_1, "volume_db", null, -12, 2, Tween.TRANS_CIRC)
-	tween.start()
+	SoundController.pub_crossfade_music_channels(0, 1)
