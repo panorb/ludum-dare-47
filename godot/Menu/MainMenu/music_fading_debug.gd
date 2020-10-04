3d6ad011ced323f71c665f371f0485ddcecf06f9
Paul Norberger <panor@fastmail.com>
2020-10-04 04:42:54 +0200

    Implemented a basic crossfade
    Not ideal, since effects layers are used for this, when ideally we should to this with multiple music layers but sound controller doesn't support that yet

--- godot/Menu/MainMenu/music_fading_debug.gd
+++ godot/Menu/MainMenu/music_fading_debug.gd
@@ -1,10 +1,19 @@
 extends Control
 
-
+onready var tween = $Tween
 
 func _on_StartMusicButton_pressed():
-	SoundController.pub_play_music("res://testmusic/song1.ogg")
+	SoundController.pub_play_effect("res://testmusic/song1.ogg", 0)
+	SoundController.pub_play_effect("res://testmusic/song2.ogg", 1, -100)
 
 
 func _on_CrossfadeButton_pressed():
-	print("Not implemented")
+	var sound_layer_0 := SoundController.pub_get_audio_effect_player(0)
+	var sound_layer_1 := SoundController.pub_get_audio_effect_player(1)
+	
+	# sound_layer_0.volume_db = -100
+	#sound_layer_1.volume_db = -100
+	
+	tween.interpolate_property(sound_layer_0, "volume_db", null, -100, 1, Tween.TRANS_CIRC)
+	tween.interpolate_property(sound_layer_1, "volume_db", null, -12, 2, Tween.TRANS_CIRC)
+	tween.start()
