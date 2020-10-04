ada7485c09d093dec0aa6c9b227eb8ca92e70525
Paul Norberger <panor@fastmail.com>
2020-10-04 14:06:03 +0200

    Revert "Further work on letters"
    This reverts commit c54b079f37cc6194140da41d9af6f7d5f3e51c24.

--- godot/Menu/LetterUI/Letter.gd
+++
@@ -1,8 +0,0 @@
-extends CenterContainer
-
-onready var letter_label := $PanelContainer/HBoxContainer/VBoxContainer/Label
-export(String) var dialouge_name := ""
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	letter_label.text = dialouge_name
