906cc3641c42e6bd3a40d65918d3d660a564b222
Paul Norberger <panor@fastmail.com>
2020-10-04 04:14:59 +0200

    Further improved main menu logical consistency
    And also removed shitton of standalone files

--- godot/Menu/MainMenu/main_menu.gd
+++ godot/Menu/MainMenu/main_menu.gd
@@ -11,8 +11,12 @@ func _on_LoadGameButton_pressed():
 
 
 func _on_CreditsButton_pressed():
-	print("Not implemented")
+	get_tree().change_scene("res://Menu/CreditMenu/CreditMenu.tscn")
 
 
 func _on_SettingsButton_pressed():
 	print("Not implemented")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
