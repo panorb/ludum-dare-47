c37b4be77ded0500d0ed893ab3584699bf3f6f36
Paul Norberger <panor@fastmail.com>
2020-10-05 15:11:28 +0200

    More letter work and hotspots and shit

--- godot/Menu/LetterUI/Letter.gd
+++ godot/Menu/LetterUI/Letter.gd
@@ -1,8 +1,12 @@
 extends PanelContainer
 
 onready var letter_label := $HBoxContainer/VBoxContainer/Label
-export(String) var letter_text := ""
+export(String) var letter_text := "" setget set_text
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	letter_label.text = letter_text
+
+func set_text(value):
+	letter_label.text = value
+	letter_text = value
--- godot/Menu/LetterUI/letter_ui.gd
+++ godot/Menu/LetterUI/letter_ui.gd
@@ -4,42 +4,44 @@ onready var animation_player := $AnimationPlayer
 onready var tween := $Tween
 onready var inbox_notification := $Notification/InboxNotification
 
-export var trans_type = Tween.TRANS_BOUNCE
+export var trans_type = Tween.TRANS_CIRC
 export(float) var notification_scaling_time := 0.3
 export(Vector2) var notifaction_max_scale := Vector2(1.02, 1.02)
 export(Vector2) var notifaction_min_scale := Vector2(0.98, 0.98)
-export(String, DIR) var letter_folder := ""
 
 var stretched := false
 
+export(Array, String, FILE, "*.txt") var available_letters = []
+var current_letter_idx := 0
+
+onready var letter = $CenterView/Letter
+
 func _ready():
-	tween.interpolate_property(inbox_notification, ":rect_scale", null, notifaction_max_scale, notification_scaling_time, trans_type)
-	tween.interpolate_property(self, "stretched", false, true, notification_scaling_time)
-	tween.start()
+	show_inbox_notification()
 
 func show_inbox_notification():
-	pass
+	tween.interpolate_property(inbox_notification, ":rect_scale", Vector2(0.01, 0.01), notifaction_min_scale, 3*notification_scaling_time, trans_type)
+	tween.start()
 
-func show_centered_letter(letter_name: String):
-	pass
-
+func show_centered_letter():
+	var current_letter : String = LetterTexts.LETTERS[current_letter_idx]
+	letter.letter_text = current_letter
 
 func _on_Tween_tween_completed(object, key):
 	if object == inbox_notification and key == ":rect_scale":
-		if stretched:
-			tween.interpolate_property(inbox_notification, "rect_scale", null, notifaction_min_scale, notification_scaling_time, trans_type)
-			tween.interpolate_property(self, "stretched", true, false, notification_scaling_time)
-		else:
+		if inbox_notification.rect_scale == notifaction_min_scale:
 			tween.interpolate_property(inbox_notification, ":rect_scale", null, notifaction_max_scale, notification_scaling_time, trans_type)
-			tween.interpolate_property(self, "stretched", false, true, notification_scaling_time)
-		tween.start()
+		else:
+			tween.interpolate_property(inbox_notification, "rect_scale", null, notifaction_min_scale, notification_scaling_time, trans_type)
 
 
 func _on_MouseHotspot_entered():
-	print("entered")
-	inbox_notification.modulate = Color.black
+	inbox_notification.modulate = Color("#963341")
 
 
 func _on_MouseHotspot_exited():
-	print("exited")
 	inbox_notification.modulate = Color.white
+
+
+func _on_MouseHotspot_is_pressed():
+	show_centered_letter()
