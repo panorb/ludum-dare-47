4b2f68f88cd8e20ac5e30706d00ae0446f4b1b4e
Paul Norberger <panor@fastmail.com>
2020-10-05 18:42:56 +0200

    Main menu latest changes
    Im too tired to look up what I did

--- godot/Menu/LetterUI/letter_ui.gd
+++ godot/Menu/LetterUI/letter_ui.gd
@@ -3,7 +3,8 @@ extends Control
 onready var animation_player := $AnimationPlayer
 onready var tween := $Tween
 onready var inbox_notification := $Notification/InboxNotification
-onready var mouse_hotspot = $MouseHotspot
+onready var letter_hotspot = $LetterHotspot
+onready var fullscreen_hotspot = $FullscreenHotspot
 
 export var trans_type = Tween.TRANS_CIRC
 export(float) var notification_scaling_time := 0.3
@@ -16,19 +17,31 @@ export(Array, String, FILE, "*.txt") var available_letters = []
 var current_letter_idx := 0
 
 onready var letter = $CenterView/Letter
+signal letter_focused
+signal letter_unfocused
 
 func show_inbox_notification():
-	inbox_notification.visible = true
-	mouse_hotspot.visible = true
 	tween.interpolate_property(inbox_notification, ":rect_scale", Vector2(0.01, 0.01), notifaction_min_scale, 3*notification_scaling_time, trans_type)
+	tween.interpolate_property(letter_hotspot, ":visible", false, true, 2*notification_scaling_time)
+	
+	yield(get_tree(), "idle_frame")
+	inbox_notification.visible = true
 	tween.start()
 
 func show_centered_letter():
+	visible = true
 	letter.visible = true
+	fullscreen_hotspot.visible = true
 	var current_letter : String = LetterTexts.LETTERS[current_letter_idx]
 	letter.letter_text = current_letter
 	
+	emit_signal("letter_focused")
 	current_letter_idx += 1
+
+func close_focused_letter():
+	visible = false
+	fullscreen_hotspot.visible = false
+	emit_signal("letter_unfocused")
 
 func _on_Tween_tween_completed(object, key):
 	if object == inbox_notification and key == ":rect_scale":
@@ -38,13 +51,18 @@ func _on_Tween_tween_completed(object, key):
 			tween.interpolate_property(inbox_notification, "rect_scale", null, notifaction_min_scale, notification_scaling_time, trans_type)
 
 
-func _on_MouseHotspot_entered():
+func _on_LetterHotspot_entered():
 	inbox_notification.modulate = Color("#963341")
 
 
-func _on_MouseHotspot_exited():
+func _on_LetterHotspot_exited():
 	inbox_notification.modulate = Color.white
 
 
-func _on_MouseHotspot_is_pressed():
+func _on_LetterHotspot_is_pressed():
+	inbox_notification.visible = false
+	letter_hotspot.visible = false
 	show_centered_letter()
+
+func _on_FullscreenHotspot_is_pressed():
+	close_focused_letter()
--- godot/Menu/MainMenu/main_menu.gd
+++ godot/Menu/MainMenu/main_menu.gd
@@ -2,6 +2,31 @@ extends Control
 
 onready var letter_timer := $LetterTimer
 onready var letter_ui := $CanvasLayer/LetterUI
+onready var full_screen_hospot := $FullScreenHotspot
+onready var video_player := $VideoPlayer
+onready var button_container := $CanvasLayer/HBoxContainer
+onready var _tween = $Tween
+
+onready var play_button = $CanvasLayer/HBoxContainer/VBoxContainer/PlayButton
+onready var volume_button = $CanvasLayer/HBoxContainer/VBoxContainer/VolumeButton
+onready var exit_button = $CanvasLayer/HBoxContainer/VBoxContainer/ExitButton
 
 func _on_LetterTimer_timeout():
 	letter_ui.show_inbox_notification()
+
+func _on_LetterUI_letter_unfocused():
+	video_player.play()
+	yield(get_tree().create_timer(0.1), "timeout")
+	video_player.visible = true
+
+func _on_VideoPlayer_finished():
+	print("_on_VideoPlayer_finished")
+	_tween.interpolate_property(button_container, ":modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), 2, Tween.TRANS_SINE)
+	_tween.start()
+
+
+func _on_Tween_tween_completed(object, key):
+	if (object == button_container and key == ":modulate"):
+		play_button.disabled = false
+		volume_button.disabled = false
+		exit_button.disabled = false
