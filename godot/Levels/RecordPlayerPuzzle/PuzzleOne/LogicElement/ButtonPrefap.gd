ff20418dc82f42a840e03e06234bb6717101ce38
Jan Schuffenhauer <explorerkiller@gmail.com>
2020-10-05 17:21:08 +0200

    fixed a bug AND included button lottie

--- godot/Levels/RecordPlayerPuzzle/PuzzleOne/LogicElement/ButtonPrefap.gd
+++ godot/Levels/RecordPlayerPuzzle/PuzzleOne/LogicElement/ButtonPrefap.gd
@@ -1,10 +1,19 @@
 extends TextureButton
 
 signal got_pressed(value)
+onready var animated_sprite = $AnimatedSprite
 
 func _ready():
 	yield(get_tree(), "idle_frame")
 	emit_signal("got_pressed",pressed)
 
 func _on_ButtonPrefap_pressed():
+	animated_sprite.play("default",pressed)
 	emit_signal("got_pressed",pressed)
+
+func _on_AnimatedSprite_animation_finished():
+	animated_sprite.stop()
+	if pressed:
+		animated_sprite.set_frame(0)
+	else:
+		animated_sprite.set_frame(30)
